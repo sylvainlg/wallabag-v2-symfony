f495a3d93c75f18c5520ff4ceb75880006b079a6
syllg <syllebreton@gmail.com>
2015-01-29 14:51:36 +0100

    suppression des fichiers d'exemple bis

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            #new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Wallabag\Bundle\CoreBundle\WallabagCoreBundle(),
             new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
